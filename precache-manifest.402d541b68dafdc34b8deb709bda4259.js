ade6c521fdd1dd6b132960e4f1712e3a00388d00
Miguel Angel <miguelapb35@gmail.com>
2020-05-06 22:11:26 -0500

    Updates

---
+++ precache-manifest.402d541b68dafdc34b8deb709bda4259.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b47412f2acdb093b8494c97c0156edf5",
+    "url": "/react-shopping-cart-checkout/index.html"
+  },
+  {
+    "revision": "9c7398b8eca8bdf6d869",
+    "url": "/react-shopping-cart-checkout/static/css/main.af6835f0.chunk.css"
+  },
+  {
+    "revision": "544f4ff3b8ab1516bb26",
+    "url": "/react-shopping-cart-checkout/static/js/2.f2aa206d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-shopping-cart-checkout/static/js/2.f2aa206d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c7398b8eca8bdf6d869",
+    "url": "/react-shopping-cart-checkout/static/js/main.21f582dc.chunk.js"
+  },
+  {
+    "revision": "0b6ec85c51661be430af",
+    "url": "/react-shopping-cart-checkout/static/js/runtime-main.7ea4ece3.js"
+  }
+]);
